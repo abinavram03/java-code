9016d065178d5c03ae8de8ecfc0e0e0d3e675f7d
abinavram03 <abinavram2007@gmail.com>
2025-11-07 21:02:10 +0530

    Add files via upload

---
+++ decision.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ class decision
+{
+    public static void main(String[] args)
+    {
+        Scanner ip=new Scanner(System.in);
+        int num=ip.nextInt();
+        {
+            if(num%2==0)
+            System.out.println("even");
+            else
+            System.out.println("odd");
+
+        }
+    }
+}
