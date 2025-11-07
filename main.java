d87b1a4e649a9b0b5a2785e0fe4ef5701c770384
abinavram03 <abinavram2007@gmail.com>
2025-11-07 20:49:36 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class main
+{
+    public static void main(String[] args) {
+        Scanner ip=new Scanner(System.in);
+        int data = ip.nextInt();
+        int res =(data - 3);
+        System.out.println(res);
+
+    }
+}
